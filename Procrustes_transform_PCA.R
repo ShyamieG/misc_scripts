1fdba2ca6ed1713c9e8cb85e9bbd2a334ae0b6d8
Shyamalika Gopalan <51133211+ShyamieG@users.noreply.github.com>
2020-08-06 14:25:20 -0700

    updated Procrustes_transform_PCA python and R scripts
    Procrustes_transform_PCA.py - minor changes, wrote 'make_PCA' function for clarity
    Procrustes_transform_PCA.R - updated procrustes() step and now use predict() with procrustes object to adjust dropped in sample values

--- Procrustes_transform_PCA.R
+++ Procrustes_transform_PCA.R
@@ -6,12 +6,11 @@ args = commandArgs(trailingOnly=TRUE)
 library(vegan)
 options(stringsAsFactors=F)
 
-filename <- paste("_eigs_", args[1], sep="")
-n_eigs <- as.numeric(args[2])
+n_eigs <- as.numeric(args[1])
 outfile <- "Procrustes_PCA_out.csv"
 
 ## Determine 'base' PCA to use - based on which individual had the most SNPs available
-SNP_counts <- read.table("dropInSamplesInfo", header=T)
+SNP_counts <- read.table("dropInSamplesInfo", header=F)
 ranked_inds <- SNP_counts[order(SNP_counts[,2], decreasing=T),1]
 base_ind = ranked_inds[1]
 
@@ -23,20 +22,21 @@ write.table(t(c("", Header[-1])), file=outfile, quote=F, sep=",", row.names=F, c
 ## Write out base PC values, including best drop in sample
 base_PC_vals <- read.table(paste(base_ind, "_PCA.eigs", sep=""), row.names=1)[,-(n_eigs+1)]
 rownames(base_PC_vals) <- unlist(strsplit(rownames(base_PC_vals), split=":"))[seq(2, nrow(base_PC_vals)*2, 2)]
-write.table(base_PC_vals, file=outfile, append=T, quote=F, sep=",", col.names=F)
+write.table(base_PC_vals, file=outfile, quote=F, sep=",", col.names=F)
 refOnly_base_PC_vals <- base_PC_vals[-match(base_ind, rownames(base_PC_vals)),]
 
 ## Perform Procrustes transformation on the rest of the drop in samples
 for (ind in ranked_inds[-1]) {
+    cat(paste("Performing Procrustes transform on", ind, "\n"))
     dropin_PC_vals <- read.table(paste(ind, "_PCA.eigs", sep=""), row.names=1)[,-(n_eigs+1)]
     rownames(dropin_PC_vals) <- unlist(strsplit(rownames(dropin_PC_vals), split=":"))[seq(2, nrow(dropin_PC_vals)*2, 2)]
     refOnly_dropin_PC_vals <- dropin_PC_vals[-match(ind, rownames(dropin_PC_vals)),]
     refOnly_dropin_PC_vals <- refOnly_dropin_PC_vals[rownames(refOnly_base_PC_vals),]
     # Determine scale, rotation, translation to minimize sum of squares w/ target points
+    Proc_transform=procrustes(X=as.matrix(refOnly_base_PC_vals), Y=as.matrix(refOnly_dropin_PC_vals))
     # adjust PCs for drop in sample by parameters determined in previous step
-    Proc_transform=procrustes(X=refOnly_base_PC_vals, Y=refOnly_dropin_PC_vals, translation=T, dilation=T)
-    output=colSums(dropin_PC_vals[match(ind, rownames(dropin_PC_vals)),] * Proc_transform$scale * Proc_transform$rotation) + (Proc_transform$translation)
-    write.table(t(c(ind, output)), file=outfile, append=T, col.names=F, row.names=F, quote=F, sep=",")
+    output=predict(Proc_transform, dropin_PC_vals[match(ind, rownames(dropin_PC_vals)),])
+    write.table(output, file=outfile, append=T, col.names=F, row.names=T, quote=F, sep=",")
 }
 
 ## fin
--- Procrustes_transform_PCA.py
+++ Procrustes_transform_PCA.py
@@ -6,12 +6,12 @@
 ##         --sample_list (a list of samples to drop in, having at least the first two columns of the .fam file)
 ## OPTIONAL ARGUMENTS:
 ##         --drop_missing_data (should SNPs not present in the sample being dropped in be removed when doing the PCA? default True)
-##         --make_pseudohap (should reference samples be made pseudohaploid, default False)
 ##         --n_eigs (number of eigenvalues to compute, default 6)
 ##         --LD_winsize (window size in SNPs - to pass to PLINK for indep-pairwise LD filter)
 ##         --LD_stepsize (step size in SNPs - to pass to PLINK for indep-pairwise LD filter)
 ##         --LD_r2 (maximum pairwise SNP correlation to tolerate  - to pass for indep-pairwise LD filter)
 ##         --maf_min (minimum minor allele frequency - to pass to PLINK for maf filter)
+##         --threads (number of CPUs to use to perform individual PCAs)
 ##
 ## USAGE EXAMPLES:
 ## $ python Procrustes_transform_PCA.py --bfile PCA_data --sample_list drop_in.list --n_eigs 10 
@@ -23,6 +23,7 @@ import sys
 
 import pandas as pd
 import numpy as np
+import multiprocessing as mp
 
 ### Define main function
 def main(args):
@@ -31,14 +32,14 @@ def main(args):
     argp.add_argument("--bfile", required=True)
     argp.add_argument("--sample_list", required=True)
     argp.add_argument("--drop_missing_data", default=True, type=bool)
-    argp.add_argument("--make_pseudohap", default=False, type=bool)
     argp.add_argument("--n_eigs", default=6, type=int)
     argp.add_argument("--LD_winsize", type=int, required=False)
     argp.add_argument("--LD_stepsize", type=int, required=False)
     argp.add_argument("--LD_r2", type=float, required=False)
     argp.add_argument("--maf_min", type=float, required=False)
+    argp.add_argument("--threads", default=2, type=int)
     args = argp.parse_args(args)
-
+    
     ## Perform a PCA for each dropped in individual
     print("Parsing input...")
     all_samples = pd.read_csv(str(args.bfile) + ".fam", sep="\s+", header=None)
@@ -49,93 +50,58 @@ def main(args):
     for i in all_samp_concatIDs:
         if i not in dropin_samp_concatIDs:
             idx_ref.append(all_samp_concatIDs.index(i))
-
     ref_samples = all_samples.iloc[idx_ref,]
-    
-    if args.make_pseudohap == True:
-        print("Making all samples pseudo-haploid...")
-        import pandas_plink as pdp
-        import random
-        bedfile = pdp.read_plink1_bin(".TEMP.bed")
-        for ind_idx in range(bedfile.shape[0]):
-            print("    working on sample " + str(bedfile.iid.values[ind_idx]) + " (" + str(ind_idx+1) + "/" + str(bedfile.shape[0]) + ")")
-            genos = bedfile.sel(sample=bedfile.sample.values[ind_idx]).values.tolist()
-            for g in (np.where(np.array(genos)==1)[0][0:]):
-                genos[g] = float(np.array(random.sample([0., 2.], k=1)))
-            alleles = [0] * len(genos)
-            for n in range(len(genos)):
-                if genos[n]==0.:
-                    alleles[n] = bedfile.a0.values[n]
-                elif genos[n]==2.:
-                    alleles[n] = bedfile.a1.values[n]
-            IND_ped = [bedfile.fid.values[ind_idx].tolist()] + [bedfile.iid.values[ind_idx].tolist()] + [bedfile.father.values[ind_idx].tolist()] + [bedfile.mother.values[ind_idx].tolist()] + [bedfile.gender.values[ind_idx].tolist()] + [str(bedfile.trait.values[ind_idx].tolist())] + [x for pair in zip(alleles, alleles) for x in pair]
-            if ind_idx==0:
-                with open(str(args.bfile) + "_pseudoHap.ped", "w") as pedfile:
-                    pedfile.write("%s" % " ".join(map(str, IND_ped)))
-            else:
-                with open(str(args.bfile) + "_pseudoHap.ped", "a") as pedfile:
-                    pedfile.write("\n" + "%s" % " ".join(map(str, IND_ped)))
-        os.system("awk \'{print $1, $2, $3, $4}\' " + str(args.bfile)+ ".bim > " + str(args.bfile) + "_pseudoHap.map")
-        args.bfile = str(args.bfile) + "_pseudoHap"
-        os.system("plink2 --file " + str(args.bfile) + " --make-bed --out " + str(args.bfile))
-        os.system("rm " + str(args.bfile) + ".ped")
-        os.system("rm " + str(args.bfile)+ ".map")
-
-    # Modify to run in parallel?
     os.system("echo sample_ID n_SNPs > dropInSamplesInfo")
     print("Doing individual PCAs...")
-    for i in range(len(dropin_samples)):
-        # Make files for individual i
-        IND_df = dropin_samples.iloc[i:i+1,]
-        ind = IND_df.iloc[0,1]
-        print("    working on sample " + str(ind) + "...")
-        rem_df = dropin_samples.drop(i)
-        rem_df.to_csv(".REM.list", sep=" ", index=False, header=False)
-        if args.drop_missing_data == True:
-            # Keep individual i, and only the SNPs typed in that individual
-            IND_df.to_csv(".KEEP.list", sep=" ", index=False, header=False)
-            os.system("plink2 --bfile " + str(args.bfile) + " --keep .KEEP.list --geno 0 --make-bed --out .KEEP")
-            # Remove everyone else that needs to be dropped in, and keep only SNPs typed in individual i
-            os.system("plink2 --bfile " + str(args.bfile) + " --remove .REM.list --extract .KEEP.bim --make-bed --out .TEMP")
-        else:
-            os.system("plink2 --bfile " + str(args.bfile) + " --remove .REM.list --make-bed --out .TEMP")
-        
-        if (args.LD_winsize is not None) & (args.LD_stepsize is not None) & (args.LD_r2 is not None):
-            os.system("plink2 --bfile .TEMP --indep-pairwise " + str(args.LD_winsize) + " " + str(args.LD_stepsize) + " " + str(args.LD_r2) + " --out .LD_filter")
-            os.system("plink2 --bfile .TEMP --extract .LD_filter.prune.in --make-bed --out .TEMP2")
-            os.system("mv .TEMP2.fam .TEMP.fam"); os.system("mv .TEMP2.bim .TEMP.bim"); os.system("mv .TEMP2.bed .TEMP.bed"); os.system("rm .TEMP2.*")
-        
-        if args.maf_min is not None:
-            os.system("plink2 --bfile .TEMP --maf " + str(args.maf_min) + " --make-bed --out .TEMP2")
-            os.system("mv .TEMP2.fam .TEMP.fam"); os.system("mv .TEMP2.bim .TEMP.bim"); os.system("mv .TEMP2.bed .TEMP.bed"); os.system("rm .TEMP2.*")
-         
-        # Create parameter file for smartpca, and run
-        os.system("echo genotypename: .TEMP.bed > .par.all")
-        os.system("echo snpname: .TEMP.bim >> .par.all")
-        os.system("echo indivname: .TEMP.fam >> .par.all")
-        os.system("echo output: " + str(ind) + "_PCA.eigs >> .par.all")
-        os.system("echo altnormstyle: NO >> .par.all")
-        os.system("echo numeigs: " + str(args.n_eigs) + " >> .par.all")
-        os.system("echo numoutlieriter: 0 >> .par.all")
-        os.system("echo numoutlierevec: 0 >> .par.all")
-        os.system("echo missingmode: NO >> .par.all")
-        os.system("echo nsnpldregress: 0 >> .par.all")
-        os.system("echo noxdata: YES >> .par.all")
-        os.system("echo nomalexhet: YES >> .par.all")
-        os.system("smartpca -p .par.all")
-        
-        # Record SNP number for each drop in sample
-        os.system("echo " + str(ind) + " `wc -l .TEMP.bim | awk '{print $1}'` >> dropInSamplesInfo")
-    
-                
+    for x in range(len(dropin_samples)):
+        make_PCA(x)
     ## Clean up
     os.system("rm .KEEP.* .REM.list .TEMP.* .par.all .LD_filter.*")
-
+    
     ## Run Procrustes transformation with R script, package "vegan"
     print("Running Procrustes transformation...")
-    os.system("Rscript Procrustes_transform_PCA.R " + str(args.bfile) + " " + str(args.n_eigs))
-
+    os.system("Rscript Procrustes_transform_PCA.R " + str(args.n_eigs))
     return 0
+
+### Define PCA function
+def make_PCA(i):
+    # Make files for individual i
+    IND_df = dropin_samples.iloc[i:i+1,]
+    ind = IND_df.iloc[0,1]
+    print("    working on sample " + str(ind) + "...")
+    rem_df = dropin_samples.drop(i)
+    rem_df.to_csv(".REM.list", sep=" ", index=False, header=False)
+    if args.drop_missing_data == True:
+        # Keep individual i, and only the SNPs typed in that individual
+        IND_df.to_csv(".KEEP.list", sep=" ", index=False, header=False)
+        os.system("plink2 --bfile " + str(args.bfile) + " --keep .KEEP.list --geno 0 --make-bed --out .KEEP")
+        # Remove everyone else that needs to be dropped in, and keep only SNPs typed in individual i
+        os.system("plink2 --bfile " + str(args.bfile) + " --remove .REM.list --extract .KEEP.bim --make-bed --out .TEMP")
+    else:
+        os.system("plink2 --bfile " + str(args.bfile) + " --remove .REM.list --make-bed --out .TEMP")
+    if (args.LD_winsize is not None) & (args.LD_stepsize is not None) & (args.LD_r2 is not None):
+        os.system("plink2 --bfile .TEMP --indep-pairwise " + str(args.LD_winsize) + " " + str(args.LD_stepsize) + " " + str(args.LD_r2) + " --out .LD_filter")
+        os.system("plink2 --bfile .TEMP --extract .LD_filter.prune.in --make-bed --out .TEMP2")
+        os.system("mv .TEMP2.fam .TEMP.fam"); os.system("mv .TEMP2.bim .TEMP.bim"); os.system("mv .TEMP2.bed .TEMP.bed"); os.system("rm .TEMP2.*")
+    if args.maf_min is not None:
+        os.system("plink2 --bfile .TEMP --maf " + str(args.maf_min) + " --make-bed --out .TEMP2")
+        os.system("mv .TEMP2.fam .TEMP.fam"); os.system("mv .TEMP2.bim .TEMP.bim"); os.system("mv .TEMP2.bed .TEMP.bed"); os.system("rm .TEMP2.*")
+    # Create parameter file for smartpca, and run
+    os.system("echo genotypename: .TEMP.bed > .par.all")
+    os.system("echo snpname: .TEMP.bim >> .par.all")
+    os.system("echo indivname: .TEMP.fam >> .par.all")
+    os.system("echo output: " + str(ind) + "_PCA.eigs >> .par.all")
+    os.system("echo altnormstyle: NO >> .par.all")
+    os.system("echo numeigs: " + str(args.n_eigs) + " >> .par.all")
+    os.system("echo numoutlieriter: 0 >> .par.all")
+    os.system("echo numoutlierevec: 0 >> .par.all")
+    os.system("echo missingmode: NO >> .par.all")
+    os.system("echo nsnpldregress: 0 >> .par.all")
+    os.system("echo noxdata: YES >> .par.all")
+    os.system("echo nomalexhet: YES >> .par.all")
+    os.system("smartpca -p .par.all")
+    # Record SNP number for each drop in sample
+    os.system("echo " + str(ind) + " `wc -l .TEMP.bim | awk '{print $1}'` >> dropInSamplesInfo")
 
 ### Run script
 if __name__ == "__main__":
