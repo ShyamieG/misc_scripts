e0e7db4bbfdbe5bcfbcc80f0d2f4fe8737d1d281
Shyamalika Gopalan <51133211+ShyamieG@users.noreply.github.com>
2021-04-21 14:14:36 -0400

    updated "help" for arguments

--- Procrustes_transform_PCA.py
+++ Procrustes_transform_PCA.py
@@ -1,19 +1,7 @@
 ## SCRIPT: Procrustes_transform_PCA.py
 ## AUTHOR: SSG
-## OVERVIEW: Produces a set of PCAs by dropping in a set of individuals one at a time
-## REQUIRED ARGUMENTS:
-##         --bfile (stem of the PLINK binary file name that contains data on both reference individuals and those to be dropped)
-##         --sample_list (a list of samples to drop in, having at least the first two columns of the .fam file)
-## OPTIONAL ARGUMENTS:
-##         --drop_missing_data (should SNPs not present in the sample being dropped in be removed when doing the PCA? default True)
-##         --n_eigs (number of eigenvalues to compute, default 6)
-##         --LD_winsize (window size in SNPs - to pass to PLINK for indep-pairwise LD filter)
-##         --LD_stepsize (step size in SNPs - to pass to PLINK for indep-pairwise LD filter)
-##         --LD_r2 (maximum pairwise SNP correlation to tolerate  - to pass for indep-pairwise LD filter)
-##         --maf_min (minimum minor allele frequency - to pass to PLINK for maf filter)
-##         --threads (number of CPUs to use to perform individual PCAs)
-##
-## USAGE EXAMPLES:
+## DESCRIPTION: Produces a set of PCAs by dropping in a set of individuals one at a time
+## USAGE EXAMPLE:
 ## $ python Procrustes_transform_PCA.py --bfile PCA_data --sample_list drop_in.list --n_eigs 10 
 
 #! /usr/bin/env python
@@ -29,15 +17,15 @@ import multiprocessing as mp
 def main(args):
     ## Parse arguments
     argp = ap.ArgumentParser(description="")
-    argp.add_argument("--bfile", required=True)
-    argp.add_argument("--sample_list", required=True)
-    argp.add_argument("--drop_missing_data", default=True, type=bool)
-    argp.add_argument("--n_eigs", default=6, type=int)
-    argp.add_argument("--LD_winsize", type=int, required=False)
-    argp.add_argument("--LD_stepsize", type=int, required=False)
-    argp.add_argument("--LD_r2", type=float, required=False)
-    argp.add_argument("--maf_min", type=float, required=False)
-    argp.add_argument("--threads", default=2, type=int)
+    argp.add_argument("--bfile", required=True, help="stem of the PLINK binary file name that contains data on both reference individuals and those to be dropped")
+    argp.add_argument("--sample_list", required=True, help="a list of samples to drop in, having at least the first two columns of the .fam file")
+    argp.add_argument("--drop_missing_data", default=True, type=bool, help="should SNPs not present in the sample being dropped in be removed when doing the PCA? default True")
+    argp.add_argument("--n_eigs", default=6, type=int, help="number of eigenvalues to compute, default 6")
+    argp.add_argument("--LD_winsize", type=int, required=False, help="window size in SNPs - to pass to PLINK for indep-pairwise LD filter")
+    argp.add_argument("--LD_stepsize", type=int, required=False, help="")
+    argp.add_argument("--LD_r2", type=float, required=False, help="")
+    argp.add_argument("--maf_min", type=float, required=False, help="")
+    argp.add_argument("--threads", default=2, type=int, help="")
     args = argp.parse_args(args)
     
     ## Perform a PCA for each dropped in individual
